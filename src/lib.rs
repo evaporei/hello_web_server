f99921ec437b361744945bfe4c16445ee66f3cff
Otávio Pace <otaviopp8@gmail.com>
2018-04-03 20:11:21 -0300

    threadpool: add workers with worker struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,49 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
-    pub fn new(size: u32) -> ThreadPool {
-        ThreadPool
+    /// Create ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool {
+            workers,
+        }
     }
 
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker {
+            id,
+            thread,
+        }
     }
 }
